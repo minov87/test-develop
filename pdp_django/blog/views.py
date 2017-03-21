1a94c9f1dbaabd859dc8fb95bec50a1ead6a9686
minov87 <minov87@gmail.com>
2017-03-21 18:19:33 +0900

    챕터 8, 챕터 9 챕터 8 - disqus 플러그인 설치 및 세팅
    챕터 9
    - 블로그 검색기능 추가

--- pdp_django/blog/views.py
+++ pdp_django/blog/views.py
@@ -7,6 +7,11 @@ from blog.models import Post
 from tagging.models import Tag, TaggedItem
 from tagging.views import TaggedObjectList
 
+from django.views.generic.edit import FormView
+from blog.forms import PostSearchForm
+from django.db.models import Q
+from django.shortcuts import render
+
 # Create your views here.
 class TagTV(TemplateView):
     template_name = 'tagging/tagging_cloud.html'
@@ -47,3 +52,19 @@ class PostTAV(TodayArchiveView):
     model = Post
     month_format = '%m' # %b 형태가 한글인 문제로 월 형태를 숫자로 변경
     date_field = 'modify_date'
+
+# Formview
+class SearchFormView(FormView):
+    form_class = PostSearchForm
+    template_name = 'blog/post_search.html'
+
+    def form_valid(self, form):
+        schWord = '%s' % self.request.POST['search_word']
+        post_list = Post.objects.filter(Q(title__icontains=schWord) |
+                    Q(discription__icontains=schWord) | Q(content__icontains=schWord)).distinct()
+        context = {}
+        context['form'] = form
+        context['search_term'] = schWord
+        context['object_list'] = post_list
+
+        return render(self.request, self.template_name, context)
