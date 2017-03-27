062898efa4d3ea8bd718266dfa8de603c5fa01f1
minov87 <minov87@gmail.com>
2017-03-27 13:09:07 +0900

    챕터 10 - 사진 앨범 리스트 및 뷰 페이지 작업

--- pdp_django/pdp_django/urls.py
+++ pdp_django/pdp_django/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     url(r'^photo/', include('photo.urls', namespace='photo')),
 
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
